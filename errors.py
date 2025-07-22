72f69d06c67e14d8d30eaf9e8ccc331aa6952f29
amirkle27 <amirkle@gmail.com>
2025-07-22 13:25:57 +0300

    Update errors.py

--- errors.py
+++ errors.py
@@ -6,3 +6,17 @@ class InvalidPenaltySolverCombination(Exception):
     def __init__(self, penalty: str, solver:str):
         """Raised when an invalid combination of penalty and solver is used in Logistic Regression"""
         super().__init__(f"Invalid combination of penalty: '{penalty}' and solver: {solver}.")
+
+class NotFittedError(Exception):
+    def __init__(self):
+        """Raised when a clustered model is not fitted correctly before predicting new data"""
+        super().__init__("Model must be fit before predicting.")
+
+class MultipleFeaturesPolyError(Exception):
+    def __init__(self):
+        """Raised when a clustered model is not fitted correctly before predicting new data"""
+        super().__init__("Cannot plot: Only works for 1 feature.")
+
+
+
+
