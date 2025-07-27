a0563b5e05113ed16f7b81e020eef91dcee08747
amirkle27 <amirkle@gmail.com>
2025-07-27 14:39:26 +0300

    Update preprocessoring_strategy.py

--- preprocessoring_strategy.py
+++ preprocessoring_strategy.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 import pandas as pd
 from errors import TargetColumnNotFoundError
 from typing import Optional
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import PolynomialFeatures, LabelEncoder
 from tensorflow.keras.preprocessing.text import Tokenizer
 import numpy as np
 from keras.utils import pad_sequences
@@ -50,6 +50,7 @@ class LogisticRegressionPreprocessor(PreprocessingStrategy):
     def process(self, df: pd.DataFrame, target_col: str) -> tuple[pd.DataFrame, pd.Series]:
         drop_cols = [col for col in df.columns if "index" in col.lower() or "id" in col.lower()]
         df = df.drop(columns=drop_cols, errors='ignore')
+        df = df.dropna()
 
         if target_col not in df.columns:
             raise TargetColumnNotFoundError(target_col)
@@ -80,6 +81,7 @@ class KNNPreprocessor(PreprocessingStrategy):
     def process(self, df: pd.DataFrame, target_col: Optional[str] = None) -> tuple[pd.DataFrame, Optional[pd.Series]]:
         drop_cols = [col for col in df.columns if "index" in col.lower() or "id" in col.lower()]
         df = df.drop(columns=drop_cols, errors='ignore')
+        df = df.dropna()
         if target_col:
             if target_col not in df.columns:
                 raise TargetColumnNotFoundError(target_col)
@@ -97,21 +99,33 @@ class KNNPreprocessor(PreprocessingStrategy):
         return X, y
 
 class ANNCNNPreprocessor(PreprocessingStrategy):
+    def __init__(self):
+        self.label_encoder = None
+
     def process(self, df: pd.DataFrame, target_col: Optional[str] = None) -> tuple[pd.DataFrame, Optional[pd.Series]]:
         drop_cols = [col for col in df.columns if "index" in col.lower() or "id" in col.lower()]
         df = df.drop(columns=drop_cols, errors='ignore')
+        df = df.dropna()
+        if target_col is not None:
+            if target_col not in df.columns:
+                raise TargetColumnNotFoundError(target_col)
+            y = df[target_col]
+            X = df.drop(columns=target_col)
 
-        if target_col not in df.columns:
-            raise TargetColumnNotFoundError(target_col)
+            str_cols = [col for col in X.columns if X[col].dtype == 'object']
+            if str_cols:
+                X = pd.get_dummies(X, columns=str_cols)
 
-        y = df[target_col]
-        X = df.drop(columns=[target_col])
+            if y.dtype == 'object':
+                le = LabelEncoder()
+                y = pd.Series(le.fit_transform(y))
 
-        str_cols = [col for col in X.columns if X[col].dtype == 'object']
-        if str_cols:
-            X = pd.get_dummies(X, columns=str_cols)
-
-        return X, y
+            return X, y
+        else:
+            str_cols = [col for col in df.columns if df[col].dtype == 'object']
+            if str_cols:
+                df = pd.get_dummies(df, columns=str_cols)
+            return df,None
 
 class NLPPreprocessor(PreprocessingStrategy):
     def __init__(self, num_words: int = 10000, max_len: int = 100):
